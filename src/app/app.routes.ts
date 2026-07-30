77f13e4c857d2e7c13f107da57379aa13ce62f11
Juawo <joaopedrojp2703@gmail.com>
2026-07-30 11:47:57 -0300

    feat: define application routes for Home and PostDetail components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from "./features/home/home";
+import { PostDetail } from "./features/post-detail/post-detail";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'post/:id', component: PostDetail },
+  { path: '**', redirectTo: '' }
+];
