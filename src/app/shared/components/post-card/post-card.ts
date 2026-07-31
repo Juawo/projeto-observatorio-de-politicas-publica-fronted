649c597a6a4bb08fd1d6f7925476832c2b9f66d9
Juawo <joaopedrojp2703@gmail.com>
2026-07-30 22:46:50 -0300

    feat: implement post card component with dynamic content and styling

--- src/app/shared/components/post-card/post-card.ts
+++ src/app/shared/components/post-card/post-card.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+export interface Post {
+  id: number;
+  title: string;
+  author: string;
+  date: string;
+  category: string;
+  imageUrl: string;
+}
 
 @Component({
   selector: 'app-post-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './post-card.html',
   styleUrl: './post-card.scss',
 })
-export class PostCard {}
+
+export class PostCard{
+  @Input({ required: true }) post!: Post;
+
+  getCategoryClass(category: string): string {
+    if (!category) return 'tag-default';
+
+    const slug = category
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/\s+/g, '-');
+
+    return `tag-${slug}`
+  }
+}
