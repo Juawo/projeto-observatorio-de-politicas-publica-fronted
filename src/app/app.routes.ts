09617783b8294d4261791ad330e06bd66c1925ac
Juawo <joaopedrojp2703@gmail.com>
2026-08-03 15:14:16 -0300

    feat: update routing and links for About page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from "./features/home/home";
 import { PostDetail } from "./features/post-detail/post-detail";
 import { Posts } from './features/posts/posts';
+import { About } from './features/about/about';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'posts', component: Posts },
-  // { path: 'about', component: SobreComponent },
+  { path: 'about', component: About },
   { path: 'post/:id', component: PostDetail },
   { path: '**', redirectTo: '' }
 ];
